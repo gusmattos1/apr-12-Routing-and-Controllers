dace5ca8af07b3c3ade37429c7d2f3fabe233be1
gusmattos1 <gusmattos@gmail.com>
2018-04-12 19:55:30 -0400

    starting assignment 3

--- assignment2/config/routes.rb
+++ assignment2/config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   get '/welcome' => 'pages#welcome'
   get '/about' => 'pages#about'
   get '/contest' => 'pages#contest'
-  get '/' => 'pages#welcome'
+
+  # get '/' => 'pages#welcome'
+  root 'pages#welcome'
+
+  get '/kitten/:size' => 'pages#kitten'
   get '/kitten/:size' => 'pages#kitten'
   get '/kittens/:size' => 'pages#kittens'
   get '/secrets/:magic_word' => 'pages#secrets'
