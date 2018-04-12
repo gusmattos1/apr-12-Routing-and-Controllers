5bd4ce2ce2b96555b2bbbf2726f3cc410d7fa699
gusmattos1 <gusmattos@gmail.com>
2018-04-12 19:45:12 -0400

    assignment 2 ongoing

---
+++ assignment2/app/controllers/pages_controller.rb
@@ -0,0 +1,34 @@
+class PagesController < ApplicationController
+
+  before_action :set_kitten_url, only: [:kitten, :kittens]
+
+
+  def welcome
+    @header = "This is the welcome page"
+  end
+
+  def kitten
+  end
+
+  def kittens
+  end
+
+  def set_kitten_url
+    requested_size = params[:size]
+    @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
+  end
+
+  def contest
+  flash[:alert] = "Sorry, the contest has ended"
+  redirect_to "/welcome"
+  end
+
+  def secrets
+
+    if params[:magic_word] != "magicword"
+      redirect_to "/welcome"
+      flash[:alert] = "Sorry, you're not authorized to see that page!"
+    end
+  end
+
+end
