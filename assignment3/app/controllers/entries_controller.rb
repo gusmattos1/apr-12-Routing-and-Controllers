49effe4c6ea92803e6382d1c88f0f06126f80d3a
gusmattos1 <gusmattos@gmail.com>
2018-04-15 15:59:14 -0400

    assignment done

--- assignment3/app/controllers/entries_controller.rb
+++ assignment3/app/controllers/entries_controller.rb
@@ -1,32 +1,56 @@
 class EntriesController < ApplicationController
 
   def index
-    # render :index
+    @entries = Entry.all
+    render :index
   end
 
-def show
-    # render :show
-end
+  def show
+      @entry = Entry.find(params[:id])
+      render :show
+  end
 
-def new
-  # render :new
-end
+  def new
+    @entry = Entry.new
+  end
 
-def create
-  redirect_to entries_url
-end
+  def create
+    @entry = Entry.new
 
-def edit
-  entrie = Entry.all
-  @entrie = entrie.find_by(params[:id])
-end
+    @entry.word = params[:entry][:word]
+    @entry.definition = params[:entry][:definition]
+    @entry.language = params[:entry][:language]
 
-def update
-redirect_to entry_url(params[:id])
-end
+    if @entry.save
+      redirect_to entries_url
+    else
+      redirect_to new_entry_url
+    end
+  end
 
-def destroy
-  redirect_to entries_url
-end
+  def edit
+    @entry = Entry.find(params[:id])
+  end
+
+  def update
+    @entry = Entry.find(params[:id])
+
+      @entry.word = params[:entry][:word]
+      @entry.definition = params[:entry][:definition]
+      @entry.language = params[:entry][:language]
+
+    if @entry.save
+      redirect_to entry_url(@entry)
+    else
+      redirect_to edit_entry_url(@entry)
+    end
+  end
+
+
+  def destroy
+    @entry = Entry.find(params[:id])
+    @entry.destroy
+    redirect_to entries_url
+  end
 
 end
